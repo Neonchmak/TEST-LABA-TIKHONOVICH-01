6faceace4bf7cb2dbcd4e46280724f10046a68bc
Тихонович Ярослав <your-email@example.com>
2026-03-12 10:13:36 +0300

    Created photoClass

---
+++ Photo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoProgram
+{
+    public class Photo
+    {
+        public string Path { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTaken { get; set; }
+        public Photo(string path, string description, DateTime dateTaken)
+        {
+            Path = path;
+            Description = description;
+            DateTaken = dateTaken;
+        }
+        public override string ToString()
+        {
+            return $"{Path} - {Description} ({DateTaken.ToString("dd.MM.yyyy")})";
+        }
+    }
+}
